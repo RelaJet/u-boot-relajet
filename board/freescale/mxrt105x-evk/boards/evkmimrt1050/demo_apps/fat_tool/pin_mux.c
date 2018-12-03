e25d4557984de8bb6bdf360222d52f712eb0ba91
Wig Cheng <wig.cheng@relajet.com>
2018-12-02 21:50:58 -0500

    mxrt105x-evk: add uart3 pinmux definition

--- board/freescale/mxrt105x-evk/boards/evkmimrt1050/demo_apps/fat_tool/pin_mux.c
+++ board/freescale/mxrt105x-evk/boards/evkmimrt1050/demo_apps/fat_tool/pin_mux.c
@@ -42,6 +42,13 @@ void BOARD_InitPins(void) {                                /*!< Function assigne
                      0U);                                    /* Software Input On Field: Input Path is determined by functionality */
     IOMUXC_SetPinMux(IOMUXC_GPIO_AD_B0_13_LPUART1_RX,        /* GPIO_AD_B0_13 is configured as LPUART1_RX */
                      0U);                                    /* Software Input On Field: Input Path is determined by functionality */
+
+
+    IOMUXC_SetPinMux(IOMUXC_GPIO_AD_B1_06_LPUART3_TX,        /* GPIO_AD_B0_12 is configured as LPUART1_TX */
+                     0U);                                    /* Software Input On Field: Input Path is determined by functionality */
+    IOMUXC_SetPinMux(IOMUXC_GPIO_AD_B1_07_LPUART3_RX,        /* GPIO_AD_B0_13 is configured as LPUART1_RX */
+                     0U);                                    /* Software Input On Field: Input Path is determined by functionality */
+
     IOMUXC_SetPinConfig(IOMUXC_GPIO_AD_B0_12_LPUART1_TX,        /* GPIO_AD_B0_12 PAD functional properties : */
                      0x10B0u);                               /* Slew Rate Field: Slow Slew Rate
                                                  Drive Strength Field: R0/6
@@ -60,6 +67,26 @@ void BOARD_InitPins(void) {                                /*!< Function assigne
                                                  Pull / Keep Select Field: Keeper
                                                  Pull Up / Down Config. Field: 100K Ohm Pull Down
                                                  Hyst. Enable Field: Hysteresis Disabled */
+
+    IOMUXC_SetPinConfig(IOMUXC_GPIO_AD_B1_06_LPUART3_TX,        /* GPIO_AD_B0_12 PAD functional properties : */
+                     0x10B0u);                               /* Slew Rate Field: Slow Slew Rate
+                                                 Drive Strength Field: R0/6
+                                                 Speed Field: medium(100MHz)
+                                                 Open Drain Enable Field: Open Drain Disabled
+                                                 Pull / Keep Enable Field: Pull/Keeper Enabled
+                                                 Pull / Keep Select Field: Keeper
+                                                 Pull Up / Down Config. Field: 100K Ohm Pull Down
+                                                 Hyst. Enable Field: Hysteresis Disabled */
+    IOMUXC_SetPinConfig(IOMUXC_GPIO_AD_B1_07_LPUART3_RX,        /* GPIO_AD_B0_13 PAD functional properties : */
+                     0x10B0u);                               /* Slew Rate Field: Slow Slew Rate
+                                                 Drive Strength Field: R0/6
+                                                 Speed Field: medium(100MHz)
+                                                 Open Drain Enable Field: Open Drain Disabled
+                                                 Pull / Keep Enable Field: Pull/Keeper Enabled
+                                                 Pull / Keep Select Field: Keeper
+                                                 Pull Up / Down Config. Field: 100K Ohm Pull Down
+                                                 Hyst. Enable Field: Hysteresis Disabled */
+
 
 #if 1
     IOMUXC_SetPinMux(IOMUXC_GPIO_SD_B0_00_USDHC1_CMD, 0);
